49e7d27ca30fa232485f28090013a6ec674e6df8
Ruslan Yun <leonemsolis@gmail.com>
2020-08-03 04:47:42 +0600

    Add static background, simple player controller script, light

---
+++ Assets/Scripts/PlayerShipController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShipController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private float speed = 80f;
+    private float tilt = .3f;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        _rb.velocity = movement * speed;
+
+        _rb.position = new Vector3 (_rb.position.x, 0.0f, _rb.position.z);
+
+        _rb.rotation = Quaternion.Euler (0.0f, 0.0f, _rb.velocity.x * -tilt);
+    }
+}
