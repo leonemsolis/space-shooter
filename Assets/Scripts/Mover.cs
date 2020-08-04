3d689f4b4d09558f851d13481246a82db10e2fde
Ruslan Yun <leonemsolis@gmail.com>
2020-08-04 11:33:06 +0600

    Add shooting, change camera to ortho

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private const float SPEED = 400f;
+    private Rigidbody _rb;
+    
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.velocity = transform.forward * SPEED;
+    }
+}
--- Assets/Scripts/PlayerShipController.cs
+++ Assets/Scripts/PlayerShipController.cs
@@ -4,24 +4,52 @@ using UnityEngine;
 
 public class PlayerShipController : MonoBehaviour
 {
+    [SerializeField] Mover bulletPrefab;
+
     private Rigidbody _rb;
-    private float speed = 80f;
-    private float tilt = .3f;
+
+    // Movement
+    private Rect bounds;
+    private const float SPEED = 100f;
+    private const float TILT = .3f;
+
+    // Fire
+    private const float FIRERATE = .25f;
+    private float nextFire = 0f;
 
     private void Start() {
         _rb = GetComponent<Rigidbody>();
+
+        bounds.xMin = -55f;
+        bounds.xMax = 55f;
+        bounds.yMin = -10f;
+        bounds.yMax = 200f;
     }
 
-    void FixedUpdate ()
+    private void Update() {
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + FIRERATE;
+            Instantiate(bulletPrefab, transform.position + new Vector3(0f, 0f, 4f), Quaternion.identity);
+        }
+    }
+
+    private void FixedUpdate ()
     {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-        _rb.velocity = movement * speed;
+        _rb.velocity = movement * SPEED;
 
-        _rb.position = new Vector3 (_rb.position.x, 0.0f, _rb.position.z);
+        _rb.position = new Vector3 
+        (
+            Mathf.Clamp (_rb.position.x, bounds.xMin, bounds.xMax), 
+            0.0f, 
+            Mathf.Clamp (_rb.position.z, bounds.yMin, bounds.yMax)
+        );
+        _rb.position = new Vector3(_rb.position.x, 0.0f, _rb.position.z);
 
-        _rb.rotation = Quaternion.Euler (0.0f, 0.0f, _rb.velocity.x * -tilt);
+        _rb.rotation = Quaternion.Euler (0.0f, 0.0f, _rb.velocity.x * -TILT);
     }
 }
