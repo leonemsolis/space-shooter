1661a7823ec9ad86c59ae4220b23656beda79bf3
Ruslan Yun <leonemsolis@gmail.com>
2020-08-04 11:38:37 +0600

    Add boundary

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Destroy(other.gameObject);
+    }
+}
